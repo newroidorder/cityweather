9d162b0abb2d9cccd7753fa8fc0770051137f0c7
newroidorder <dansantiago1998@gmail.com>
2022-03-30 05:16:17 -0300

    delay keyboard

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const city = document.getElementById('city')
 const feelsLike = document.getElementById('feelsLike')
 const current = document.getElementById('current')
 btn.addEventListener("click", getWeather, false)
+search.addEventListener("keyup", getWeather);
 
 async function  getWeather() {
 let reply = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=23702d1a64e58bc74e8cd3365105d132`);
 let info = await reply.json();
-city.innerHTML = (info.name);
+setTimeout(() => {
+    city.innerHTML = (info.name);
 current.innerHTML = (info.main.temp);
 feelsLike.innerHTML = (info.main.feels_like);
+}, 1500);
+
 }
