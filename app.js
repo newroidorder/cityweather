1ed43a9ccab028d20a1eb4131f80af73dde2bcc6
newroidorder <dansantiago1998@gmail.com>
2022-03-30 04:40:16 -0300

    versao 1

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const feelsLike = document.getElementById('feelsLike')
 const current = document.getElementById('current')
 btn.addEventListener("click", getWeather, false)
 
-function getWeather() {
-
-console.log((fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&metric&appid=23702d1a64e58bc74e8cd3365105d132`,)))
-
+async function  getWeather() {
+let reply = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=23702d1a64e58bc74e8cd3365105d132`);
+let info = await reply.json();
+city.innerHTML = (info.name);
+current.innerHTML = (info.main.temp);
+feelsLike.innerHTML = (info.main.feels_like);
 }
