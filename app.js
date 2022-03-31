744588d6bf75a4b59b5afa11e92ace173386345f
newroidorder <dansantiago1998@gmail.com>
2022-03-31 00:42:33 -0300

    versão melhorada

--- app.js
+++ app.js
@@ -3,16 +3,24 @@ const search = document.getElementById('searchUser')
 const city = document.getElementById('city')
 const feelsLike = document.getElementById('feelsLike')
 const current = document.getElementById('current')
-btn.addEventListener("click", getWeather, false)
-search.addEventListener("keyup", getWeather);
+const min = document.getElementById('min')
+const max = document.getElementById('max')
+const temperature = document.getElementById('tempera')
+btn.addEventListener("click", getWeather);
+search.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      getWeather()
+    }
+});
 
 async function  getWeather() {
-let reply = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=23702d1a64e58bc74e8cd3365105d132`);
+let reply = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&lang=pt_br&appid=23702d1a64e58bc74e8cd3365105d132`);
 let info = await reply.json();
-setTimeout(() => {
-    city.innerHTML = (info.name);
-current.innerHTML = (info.main.temp);
-feelsLike.innerHTML = (info.main.feels_like);
-}, 1500);
 
+city.innerHTML = (info.name);
+current.innerHTML = (info.main.temp);
+temperature.innerHTML = (info.main.temp);
+feelsLike.innerHTML = (info.main.feels_like);
+min.innerHTML = (info.main.temp_min);
+max.innerHTML = (info.main.temp_max);
 }
